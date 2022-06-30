02e54a642e6b495e9b21d871463b299e092c1e46
kwlaial <85923968+kwlaial@users.noreply.github.com>
2022-07-01 00:15:21 +0800

    Update preprocessor.py

--- src/utils/preprocessor.py
+++ src/utils/preprocessor.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy.misc import imread, imresize
+import imageio
+from PIL import Image
 
 
 def preprocess_input(x, v2=True):
@@ -12,11 +13,11 @@ def preprocess_input(x, v2=True):
 
 
 def _imread(image_name):
-        return imread(image_name)
+        return imageio.imread(image_name)
 
 
 def _imresize(image_array, size):
-        return imresize(image_array, size)
+        return np.array(Image.fromarray(image_array).resize(size))
 
 
 def to_categorical(integer_classes, num_classes=2):
